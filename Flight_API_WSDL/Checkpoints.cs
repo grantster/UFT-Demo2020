b9503dbd8233d1b8ff14675f50d915837f924620
Admin Default <admin@default.com>
2020-01-28 03:33:51 -0700

    price checkpoin change for 2 tickets

--- Flight_API_WSDL/Checkpoints.cs
+++ Flight_API_WSDL/Checkpoints.cs
@@ -137,7 +137,7 @@ namespace Script
             IVTDSetter actualValueSetter3 = new VTDCheckpointSetter(XmlTypeCode.Float);
             BindDirection actualBindDirection3 = new BindDirection(_flow.StServiceCallActivity6, actualValueGetter3, actualValueSetter3);
             CpValObj valueActualObject3 = new CpValObj(actualBindDirection3, XmlTypeCode.Float, false);
-            CpValObj valueExpectedObject3 = new CpValObj("500", XmlTypeCode.Float);
+            CpValObj valueExpectedObject3 = new CpValObj("1000", XmlTypeCode.Float);
             ValueCPRule cpRule3 = new ValueCPRule(valueActualObject3, valueExpectedObject3, PrimitiveTypeCP.SmallerThan, "<", false);
             activity.AddCheckpointRule(cpRule3);
 
